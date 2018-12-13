ce563c2bfad7eb5d3e37db3af22e6f8badd9994f
Gunnem <sp3dng9@gmail.com>
2018-12-12 20:23:19 -0800

    jsfile modded

--- resv.js
+++ resv.js
@@ -14,81 +14,107 @@ app.use(express.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-var characters = [
-  {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
-];
+var inputs = {
 
+    reservations: [],
+    waitlist: [],
+ 
+};
+
+var visitorCount = 0;
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
+  vistorCount++;
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-// Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+// get reservation data with api
+
+app.get("/api/tables", function(req, res) {
+  res.json(data.reservations);
+
+});
+// for waitlist
+app.get("/api/waitlist", function(req, res) {
+    res.json(data.waitlist);
+  
+  });
+
+
+// return tables and waitlist arrays
+app.get("/api/", function(req, res) {
+    res.json(data);
 });
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
+app.get("/api/vistors", function(req, res) {
+    res.json(vistorCount);
 });
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newcharacter = req.body;
+// getting table data from post
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newcharacter);
-
-  characters.push(newcharacter);
-
-  res.json(newcharacter);
-});
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+app.post("/api/new", function(req, res) {
+    var tableData = req.body;
+    console.log(tableData);
+    if (tableData && tableData.name) {
+        tableData.routeName = tableData.name.replace(/\s+/g, "").toLowerCase();
+    }
+    console.log(tableData);
+   
+    if (data.reservations.length < 5) {
+        data.reservations.push(tableData);
+    } else {
+        data.waitlist.push(tableData);
+    }
+   
+   
+    res.json(tableData);
+   });
+   
+   app.get("/api/remove/:id?", function(req, res) {
+    var tableId = req.params.id;
+   
+    if (tableId) {
+      console.log(tableId);
+       for (var i = 0; i < data.reservations.length; i++) {
+         if (tableId === data.reservations[i].id) {
+             data.reservations.splice(i, 1);
+             if (data.waitlist.length > 0) {
+                 var tempTable = data.waitlist.splice(0, 1)[0];
+                 data.reservations.push(tempTable);
+             }
+   
+           return res.json(true);
+         }
+       }
+       for (var i = 0; i < data.waitlist.length; i++) {
+         if (tableId === data.waitlist[i].id) {
+             data.waitlist.splice(i, 1);
+   
+           return res.json(true);
+         }
+       }
+       return res.json(false);
+    }
+    return res.json(false);
+   });
+   
+   // Start the Server
+   app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+   });
